b3bc16504a15abb7eaad76243b79333c88f14a4b
Wizk <ilyas.rharbaoui@gmail.com>
2017-02-10 16:35:48 +0100

    ChatBox

---
+++ Assets/ChatBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatBox : MonoBehaviour
+{
+    private string messages;
+    private string messageToSend;
+
+    private Rect windowChatBox = new Rect(Screen.width - 200, Screen.height - 400, 200, 400);
+
+    private void OnGUI()
+    {
+        windowChatBox = GUI.Window(0, windowChatBox, WindowChatBox, "ChatBox");
+    }
+
+    private void WindowChatBox(int id)
+    {
+        GUILayout.Box(messages, GUILayout.Height(350));
+
+        GUILayout.BeginHorizontal();
+        messageToSend = GUILayout.TextField(messageToSend);
+
+        if (GUILayout.Button("Send"))
+        {
+            
+        }
+
+        GUILayout.EndHorizontal();
+
+        GUI.DragWindow(new Rect(0 , 0, Screen.width, Screen.height));
+    }
+}
+
--- Assets/Networking.cs
+++ Assets/Networking.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Networking : MonoBehaviour
 {
-    private string sessionName = "game";
+    private string serverName = "game";
     private string maxPlayer = "16";
-    private string port = "25004";
+    private string port = "25566";
+
+    private Rect windowHost = new Rect(Screen.width - 300, 0, 300, Screen.height);
+    private Rect windowError = new Rect(Screen.width / 2 - 150, Screen.height / 2 - 75, 300, 150);
+    private bool serverError = false;
 
     private void Awake()
     {
@@ -15,10 +19,17 @@ public class Networking : MonoBehaviour
 
     private void OnGUI()
     {
-        if (!Network.isServer)
+        windowHost = GUI.Window(0, windowHost, WindowHost, "Servers");
+
+        if (serverError)
         {
-            GUILayout.Label("Session name : ");
-            sessionName = GUILayout.TextField(sessionName);
+            windowError = GUI.Window(1, windowError, WindowError, "Connection Error");
+        }
+
+        if (Network.peerType == NetworkPeerType.Disconnected)
+        {
+            GUILayout.Label("Server name : ");
+            serverName = GUILayout.TextField(serverName);
 
             GUILayout.Label("Max player : ");
             maxPlayer = GUILayout.TextField(maxPlayer);
@@ -26,15 +37,20 @@ public class Networking : MonoBehaviour
             GUILayout.Label("Port : ");
             port = GUILayout.TextField(port);
 
-            if (GUILayout.Button("Host"))
+            if (GUILayout.Button("Creat server"))
             {
+                bool haveNat = Network.HavePublicAddress();
+
+                /*ConnectionTesterStatus status = Network.TestConnection();
+                print(status.ToString());*/
+
                 Network.InitializeSecurity();
-                Network.InitializeServer(int.Parse(maxPlayer), int.Parse(port), !Network.HavePublicAddress());
+                Network.InitializeServer(int.Parse(maxPlayer), int.Parse(port), !haveNat);
                 Debug.Log("Server started at " + Network.player.externalIP + " port " + Network.player.externalPort);
 
                 /*MasterServer.ipAddress = "127.0.0.1";
                 MasterServer.port = 25000;*/
-                MasterServer.RegisterHost("PVP", sessionName);
+                MasterServer.RegisterHost("PVP", serverName);
             }
         }
         else
@@ -44,5 +60,44 @@ public class Networking : MonoBehaviour
                 Network.Disconnect();
             }
         }
+    }
+
+    private void WindowHost(int id)
+    {
+        if (GUILayout.Button("Refresh"))
+        {
+            MasterServer.RequestHostList("PVP");
+        }
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Box("Server name");
+        GUILayout.EndHorizontal();
+
+        if (MasterServer.PollHostList().Length != 0)
+        {
+            HostData[] data = MasterServer.PollHostList();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                GUILayout.Box(data[i].gameName);
+                if (GUILayout.Button("Connect"))
+                {
+                    Network.Connect(data[i]);
+                }
+            }
+        }
+    }
+
+    private void WindowError(int id)
+    {
+        GUILayout.Label("No network connection");
+
+        if (GUILayout.Button("ok"))
+        {
+            Network.Disconnect();
+            serverError = false;
+        }
+
+        GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
     }
 }
