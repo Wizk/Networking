a28b154e26a95b04ffda81d62e0af7e05fbfa442
Wizk <Rirerire1421>
2017-02-11 19:36:33 +0100

    Master Server Connection + Creat Server + Join Server

--- Assets/ChatBox.cs
+++ Assets/ChatBox.cs
@@ -2,33 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#pragma warning disable 618
+
 public class ChatBox : MonoBehaviour
 {
-    private string messages;
-    private string messageToSend;
+	[SerializeField] private GUIStyle style;
 
-    private Rect windowChatBox = new Rect(Screen.width - 200, Screen.height - 400, 200, 400);
+    private string msgs = "";
+	private string msgToSend = "";
+
+    private Rect windowChatBox = new Rect(Screen.width - 250, Screen.height - 300, 250, 300);
+	private NetworkView netView;
+
+	private void Awake()
+	{
+		netView = GetComponent<NetworkView> ();
+	}
 
     private void OnGUI()
     {
-        windowChatBox = GUI.Window(0, windowChatBox, WindowChatBox, "ChatBox");
+        windowChatBox = GUI.Window(2, windowChatBox, WindowChatBox, "ChatBox");
     }
 
     private void WindowChatBox(int id)
     {
-        GUILayout.Box(messages, GUILayout.Height(350));
+		GUILayout.Box(msgs, style, GUILayout.Height(250));
 
         GUILayout.BeginHorizontal();
-        messageToSend = GUILayout.TextField(messageToSend);
+		msgToSend = GUILayout.TextField(msgToSend, GUILayout.MaxWidth(170));
 
         if (GUILayout.Button("Send"))
         {
-            
+			if (msgToSend != "") 
+			{
+				netView.RPC ("SendMsg", RPCMode.All, "user : " + msgToSend + "\n");
+				msgToSend = "";
+			}
         }
 
         GUILayout.EndHorizontal();
 
-        GUI.DragWindow(new Rect(0 , 0, Screen.width, Screen.height));
+		GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
     }
+
+	[RPC]
+	private void SendMsg(string message)
+	{
+		msgs += message;
+	}
 }
 
---
+++ Assets/NetworkSetup.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEditor;
+
+[AddComponentMenu ("Network/NetworkSetup")]
+public class NetworkSetup : MonoBehaviour
+{
+	[SerializeField] public string onlineScenePath;
+	[SerializeField] private GameObject player;
+
+    private string serverName = "game";
+    private string maxPlayer = "16";
+    private string port = "25000";
+
+    private Rect windowHost = new Rect(Screen.width - 300, 0, 300, Screen.height);
+    private Rect windowError = new Rect(Screen.width / 2 - 150, Screen.height / 2 - 75, 300, 150);
+    
+	private bool serverError = false;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	private void Start()
+	{
+		InitializeMasterServer ();
+		MasterServer.RequestHostList("PVP");
+	}
+
+    private void OnGUI()
+    {
+        windowHost = GUI.Window(0, windowHost, WindowHost, "Servers");
+
+        if (serverError)
+        {
+            windowError = GUI.Window(1, windowError, WindowError, "Connection Error");
+        }
+
+        if (Network.peerType == NetworkPeerType.Disconnected)
+        {
+            GUILayout.Label("Server name : ");
+            serverName = GUILayout.TextField(serverName);
+
+            GUILayout.Label("Max player : ");
+            maxPlayer = GUILayout.TextField(maxPlayer);
+
+            GUILayout.Label("Port : ");
+            port = GUILayout.TextField(port);
+
+            if (GUILayout.Button("Creat server"))
+            {
+                bool haveNat = Network.HavePublicAddress();
+
+                /*ConnectionTesterStatus status = Network.TestConnection();
+                print(status.ToString());*/
+
+                Network.InitializeSecurity();
+                Network.InitializeServer(int.Parse(maxPlayer), int.Parse(port), !haveNat);
+                Debug.Log("Server started at " + Network.player.externalIP + " port " + Network.player.externalPort);
+
+                MasterServer.RegisterHost("PVP", serverName);
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("Disconnect"))
+            {
+                Network.Disconnect();
+            }
+        }
+    }
+
+    private void WindowHost(int id)
+    {
+        if (GUILayout.Button("Refresh"))
+        {
+            MasterServer.RequestHostList("PVP");
+        }
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Box("Server name");
+        GUILayout.EndHorizontal();
+
+        if (MasterServer.PollHostList().Length != 0)
+        {
+            HostData[] data = MasterServer.PollHostList();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+				if (GUILayout.Button(data[i].gameName))
+                {
+					string serverIp = "";
+					for(int j = 0; j < data[i].ip.Length; j++)
+					{
+						serverIp += data [i].ip [j] + " ";
+					}
+
+					Network.Connect(serverIp, data[i].port);
+					Debug.Log ("Connect to host : " + serverIp + "; port : " + data[i].port);
+                }
+            }
+        }
+
+		GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
+    }
+
+    private void WindowError(int id)
+    {
+        GUILayout.Label("No network connection");
+
+        if (GUILayout.Button("ok"))
+        {
+            Network.Disconnect();
+            serverError = false;
+        }
+
+        GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
+    }
+
+	private void InitializeMasterServer()
+	{
+		MasterServer.ipAddress = "88.173.176.162"; 
+		MasterServer.port = 23466;
+	}
+}
+
+[CustomEditor(typeof(NetworkSetup), true)]
+public class NetworkSetupEditor : Editor
+{
+	private SerializedProperty playerProp;
+	private SerializedProperty onlineScenePathProp;
+
+	void OnEnable () 
+	{
+		playerProp = serializedObject.FindProperty ("player");
+		onlineScenePathProp = serializedObject.FindProperty ("onlineScenePath");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		SceneAsset oldScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(onlineScenePathProp.stringValue);
+
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField (playerProp);
+		EditorGUI.BeginChangeCheck();
+		SceneAsset newScene = EditorGUILayout.ObjectField("Online Scene", oldScene, typeof(SceneAsset), false) as SceneAsset;
+
+		if (EditorGUI.EndChangeCheck())
+		{
+			string newPath = AssetDatabase.GetAssetPath(newScene);
+			SerializedProperty scenePathProperty = serializedObject.FindProperty("onlineScenePath");
+			scenePathProperty.stringValue = newPath;
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
